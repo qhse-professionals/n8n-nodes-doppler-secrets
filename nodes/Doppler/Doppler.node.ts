65a855e283a9cac2b487829a60998c4adb943148
Devin W. Leaman <git@4lch4.email>
2023-11-08 20:18:00 -0600

    feat: added options for Project & Config

--- nodes/Doppler/Doppler.node.ts
+++ nodes/Doppler/Doppler.node.ts
@@ -1,41 +1,49 @@
-import { INodeType, INodeTypeDescription } from 'n8n-workflow';
-import { shareFields, shareOperations } from './descriptions';
+import { INodeType, INodeTypeDescription } from 'n8n-workflow'
+// import { shareFields, shareOperations } from './descriptions'
 
 export class Doppler implements INodeType {
-	description : INodeTypeDescription = {
-		displayName: 'Doppler',
-		name: 'Doppler',
-		icon: 'file:doppler.svg',
-		group: ['transform'],
-		version: 1,
-		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
-		description: 'Interact with the Doppler API',
-		defaults: {
-			name: 'Doppler',
-		},
-		inputs: ['main'],
-		outputs: ['main'],
-		requestDefaults: {
-			baseURL: 'https://api.doppler.com/v1',
-			headers: {},
-		},
-		properties : [
-			{
-				displayName: 'Resource',
-				name: 'resource',
-				type: 'options',
-				noDataExpression: true,
-				options: [
-					{
-						name: 'Share',
-						value: 'share',
-					},
-				],
-				default: 'share',
-			},
-
-			...shareOperations,
-			...shareFields,
-		],
-	};
+  description: INodeTypeDescription = {
+    displayName: 'Doppler',
+    name: 'doppler',
+    icon: 'file:doppler.svg',
+    group: ['transform'],
+    version: 1,
+    subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+    description: 'Interact with the Doppler API.',
+    defaults: {
+      name: 'Doppler',
+    },
+    inputs: ['main'],
+    outputs: ['main'],
+    requestDefaults: {
+      baseURL: 'https://api.doppler.com/v1',
+      headers: {},
+    },
+    properties: [
+      {
+        displayName: 'Project',
+        name: 'project',
+        type: 'string',
+        default: 'n8n',
+        routing: {
+          send: {
+            property: 'project',
+            type: 'query',
+          },
+        },
+      },
+      {
+        displayName: 'Config',
+        name: 'config',
+        type: 'string',
+        default: 'nonprod',
+        routing: {
+          send: {
+            property: 'config',
+            type: 'query',
+          },
+        },
+      },
+    ],
+  }
 }
