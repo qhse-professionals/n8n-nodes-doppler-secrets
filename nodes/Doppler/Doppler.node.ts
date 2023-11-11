65afcc5fa62c69da61b1b73b8e28ca5a3002051c
Devin W. Leaman <git@4lch4.email>
2023-11-10 20:08:24 -0600

    chore: removed test prop

--- nodes/Doppler/Doppler.node.ts
+++ nodes/Doppler/Doppler.node.ts
@@ -47,21 +47,6 @@ export class Doppler implements INodeType {
               },
             },
           },
-          {
-            name: 'List Config Secrets 2',
-						value: 'listConfigSecrets2',
-						action: 'Returns all the secrets for a config in a project',
-            routing: {
-              request: {
-                method: 'GET',
-                url: '/v3/configs/config/secrets',
-                qs: {
-                  project: '={{ encodeURIComponent($parameter.project) }}',
-                  config: '={{ encodeURIComponent($parameter.config) }}',
-                },
-              },
-            },
-          }
         ],
       },
 
